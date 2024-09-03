0c967160f70b2533f1a3d8272d0ebff20d43f7be
kimzoo2 <kimzoo1207@gmail.com>
2024-09-03 16:41:17 +0900

    프로젝트 최초 세팅

---
+++ src/main/java/com/eggmeonina/scrumble/ScrumbleApplication.java
@@ -0,0 +1,13 @@
+package com.eggmeonina.scrumble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eggmeonina/scrumble/ScrumbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eggmeonina.scrumble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
