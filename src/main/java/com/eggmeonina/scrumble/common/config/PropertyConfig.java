7729dedca7bb9a40c137933319e222ea2b1c9d89
kimzoo2 <kimzoo1207@gmail.com>
2024-09-09 21:47:53 +0900

    Chore: 구글 설정 정보 추가

---
+++ src/main/java/com/eggmeonina/scrumble/common/config/PropertyConfig.java
@@ -0,0 +1,17 @@
+package com.eggmeonina.scrumble.common.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.eggmeonina.scrumble.ScrumbleApplication;
+
+@Configuration
+@PropertySource(
+	value =
+		{"classpath:/property/google-oauth.properties"},
+	ignoreResourceNotFound = true
+)
+@ConfigurationPropertiesScan(basePackageClasses = ScrumbleApplication.class)
+public class PropertyConfig {
+}
