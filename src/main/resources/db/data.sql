102d344667f0a846bfdc6ab3ab1675f603a0e9c3
kimzoo2 <kimzoo1207@gmail.com>
2024-09-11 21:07:43 +0900

    Chore: 초기 데이터 생성 sql 추가

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,2 @@
+insert into member (member_id, email, name, member_status, oauth_type) values (default, 'testA@email.com', 'testA', 'JOIN', 'GOOGLE');
+insert into member (member_id, email, name, member_status, oauth_type) values (default, 'scrumble@email.com', 'scrumble', 'JOIN', 'GOOGLE');
