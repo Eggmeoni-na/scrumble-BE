ac0c9c5f2c1aae9619b011b0a971077b1f13ba44
kimzoo2 <kimzoo1207@gmail.com>
2024-09-13 16:18:32 +0900

    Feat: 세션 통해서 로그인 유저 바인딩되도록 인증 책임 분리

--- src/main/java/com/eggmeonina/scrumble/common/resolver/MemberArgumentResolver.java
+++ src/main/java/com/eggmeonina/scrumble/common/resolver/MemberArgumentResolver.java
@@ -7,26 +7,30 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 import com.eggmeonina.scrumble.common.anotation.Member;
+import com.eggmeonina.scrumble.domain.auth.dto.LoginMember;
 import com.eggmeonina.scrumble.domain.member.domain.SessionKey;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
+		log.info("supportsParameter 실행");
 		boolean hasMemberAnnotation = parameter.hasParameterAnnotation(Member.class);
-		boolean hasLoginUserType = Member.class.isAssignableFrom(parameter.getParameterType());
+		boolean hasLoginUserType = LoginMember.class.isAssignableFrom(parameter.getParameterType());
 		return hasMemberAnnotation && hasLoginUserType;
 	}
 
 	@Override
 	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
 		HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
 		HttpSession session = request.getSession(false);
 		if (session == null) {
-			return null;
+			throw new RuntimeException("로그인하지 않은 회원입니다.");
 		}
 		return session.getAttribute(SessionKey.LOGIN_USER.name());
 	}
