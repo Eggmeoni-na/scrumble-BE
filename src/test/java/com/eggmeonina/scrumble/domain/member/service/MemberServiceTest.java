4347ec08cff3d15c4cf3209b1ce31dbb461b9c09
kimzoo2 <kimzoo1207@gmail.com>
2024-09-13 17:02:41 +0900

    Test: 회원 조회 기능 테스트 추가

--- src/test/java/com/eggmeonina/scrumble/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/eggmeonina/scrumble/domain/member/service/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package com.eggmeonina.scrumble.domain.member.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.SoftAssertions.*;
 
 
@@ -13,6 +14,7 @@ import com.eggmeonina.scrumble.domain.auth.domain.MemberInformation;
 import com.eggmeonina.scrumble.domain.auth.domain.OauthType;
 import com.eggmeonina.scrumble.domain.auth.dto.LoginMember;
 import com.eggmeonina.scrumble.domain.member.domain.Member;
+import com.eggmeonina.scrumble.domain.member.dto.MemberResponse;
 import com.eggmeonina.scrumble.domain.member.repository.MemberRepository;
 
 @SpringBootTest
@@ -26,8 +28,8 @@ class MemberServiceTest {
 	private MemberRepository memberRepository;
 
 	@Test
-	@DisplayName("로그인 요청 시 조회한 회원이 없으면 회원가입 후 회원을 반환한다.")
-	void login_success_returnsMember() {
+	@DisplayName("로그인 요청 시 조회한 회원이 없으면 회원가입 후 회원을 반환한다")
+	void login_join_success_returnsMember() {
 		// given
 		MemberInformation request = new MemberInformation("123456789", "test@naver.com", "testName", "");
 
@@ -41,6 +43,51 @@ class MemberServiceTest {
 			softly.assertThat(loginMember.getMemberId()).isEqualTo(foundMember.getId());
 			softly.assertThat(loginMember.getEmail()).isEqualTo(foundMember.getEmail());
 		});
+	}
+
+	@Test
+	@DisplayName("로그인 요청 시 조회한 회원이 있으면 회원을 반환한다")
+	void login_success_returnsMember() {
+		// given
+		MemberInformation request = new MemberInformation("123456789", "test@naver.com", "testName", "");
+
+		memberRepository.save(MemberInformation.to(request, OauthType.GOOGLE));
+
+		// when
+		LoginMember loginMember = memberService.login(request, OauthType.GOOGLE);
+
+		Member foundMember = memberRepository.findByOauthId(request.getOauthId()).get();
+
+		// then
+		assertSoftly(softly -> {
+			softly.assertThat(loginMember.getMemberId()).isEqualTo(foundMember.getId());
+			softly.assertThat(loginMember.getEmail()).isEqualTo(foundMember.getEmail());
+		});
+	}
+
+	@Test
+	@DisplayName("회원을 조회한다")
+	void findMember_success_returnsMemberResponse() {
+		// given
+		MemberInformation request = new MemberInformation("123456789", "test@naver.com", "testName", "");
+
+		Member newMember = MemberInformation.to(request, OauthType.GOOGLE);
+		memberRepository.save(newMember);
+
+		// when
+		MemberResponse response = memberService.findMember(newMember.getId());
+
+		// then
+		assertThat(response.getOauthType()).isEqualTo(newMember.getOauthInformation().getOauthType());
+		assertThat(response.getEmail()).isEqualTo(newMember.getEmail());
+	}
+
+	@Test
+	@DisplayName("회원 조회 시, 존재하지 않아 예외가 발생한다")
+	void findMember_fail_throwsException() {
+		// when
+		assertThatThrownBy(() ->memberService.findMember(1L))
+			.isInstanceOf(RuntimeException.class);
 	}
 
 }
