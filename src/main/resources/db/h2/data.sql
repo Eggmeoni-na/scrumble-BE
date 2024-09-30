8d34cec8d89155f50ddc2a70a0bd6e1760fc3ba4
kimzoo2 <kimzoo1207@gmail.com>
2024-09-30 15:45:45 +0900

    Chore: 투두 도메인 스키마 추가 및 생성 sql 추가

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -1,7 +1,26 @@
-insert into member (member_id, email, name, member_status, oauth_type) values (default, 'testA@email.com', 'testA', 'JOIN', 'GOOGLE');
-insert into member (member_id, email, name, member_status, oauth_type) values (default, 'scrumble@email.com', 'scrumble', 'JOIN', 'GOOGLE');
+insert into member (member_id, email, name, member_status, oauth_type)
+values (default, 'testA@email.com', 'testA', 'JOIN', 'GOOGLE');
+insert into member (member_id, email, name, member_status, oauth_type)
+values (default, 'scrumble@email.com', 'scrumble', 'JOIN', 'GOOGLE');
 
-insert into squad (squad_id, squad_name, deleted_flag, created_at) values ( default, '테스트 스쿼드', false, now());
+insert into squad (squad_id, squad_name, deleted_flag, created_at)
+values (default, '테스트 스쿼드', false, now());
 
-insert into squad_member(squad_member_id, member_id, squad_id, squad_member_role, squad_member_status, created_at) values ( default, 1, 1, 'LEADER', 'JOIN', now());
-insert into squad_member(squad_member_id, member_id, squad_id, squad_member_role, squad_member_status, created_at) values ( default, 2, 1, 'NORMAL', 'JOIN', now());
+insert into squad_member(squad_member_id, member_id, squad_id, squad_member_role, squad_member_status, created_at)
+values (default, 1, 1, 'LEADER', 'JOIN', now());
+insert into squad_member(squad_member_id, member_id, squad_id, squad_member_role, squad_member_status, created_at)
+values (default, 2, 1, 'NORMAL', 'JOIN', now());
+
+insert into todo(todo_id, todo_type, contents, todo_status, todo_at, deleted_flag, member_id, created_at)
+values (default, 'DAILY', '테스트 투두', 'PENDING', now(), false, 1, now());
+insert into todo(todo_id, todo_type, contents, todo_status, todo_at, deleted_flag, member_id, created_at)
+values (default, 'DAILY', '삭제된 투두', 'PENDING', now(), true, 1, now());
+insert into todo(todo_id, todo_type, contents, todo_status, todo_at, deleted_flag, member_id, created_at)
+values (default, 'DAILY', '완료된 투두', 'COMPLETED', now(), false, 1, now());
+
+insert into squad_todo(squad_todo_id, todo_id, squad_id, deleted_flag, created_at)
+values (default, 1, 1, 0, now());
+insert into squad_todo(squad_todo_id, todo_id, squad_id, deleted_flag, created_at)
+values (default, 2, 1, 0, now());
+insert into squad_todo(squad_todo_id, todo_id, squad_id, deleted_flag, created_at)
+values (default, 3, 1, 0, now());
