7563a3652c5c88f87d25d025d6f305d781e705de
kimzoo2 <kimzoo1207@gmail.com>
2024-10-08 11:51:24 +0900

    Chore: CORS 설정 추가

--- src/main/java/com/eggmeonina/scrumble/common/config/CorsConfig.java
+++ src/main/java/com/eggmeonina/scrumble/common/config/CorsConfig.java
@@ -5,18 +5,23 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.eggmeonina.scrumble.common.properties.CorsProperties;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Profile("dev")
+@RequiredArgsConstructor
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+	private final CorsProperties corsProperties;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-			.allowedOrigins
-				(
-					"https://egg-scrumble.vercel.app",
-					"https://scrumble-k-jeonghee-kimjeonghees-projects.vercel.app",
-					"http://localhost:8080"
-				)
+			.allowedOrigins(corsProperties.getOriginArr())
 			.allowCredentials(true);
 	}
 }
