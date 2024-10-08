e2e8496a57cd6049729f26ed446531fede60e6b7
kimzoo2 <kimzoo1207@gmail.com>
2024-10-08 15:51:10 +0900

    Test: 스쿼드 투두 페이징 테스트 추가

--- src/test/java/com/eggmeonina/scrumble/domain/todo/service/SquadTodoServiceIntegrationTest.java
+++ src/test/java/com/eggmeonina/scrumble/domain/todo/service/SquadTodoServiceIntegrationTest.java
@@ -22,6 +22,8 @@ import com.eggmeonina.scrumble.domain.todo.domain.ToDo;
 import com.eggmeonina.scrumble.domain.todo.domain.TodoStatus;
 import com.eggmeonina.scrumble.domain.todo.dto.SquadTodoRequest;
 import com.eggmeonina.scrumble.domain.todo.dto.SquadTodoResponse;
+import com.eggmeonina.scrumble.domain.todo.dto.ToDoRequest;
+import com.eggmeonina.scrumble.domain.todo.dto.ToDoResponse;
 import com.eggmeonina.scrumble.domain.todo.repository.SquadTodoRepository;
 import com.eggmeonina.scrumble.domain.todo.repository.TodoRepository;
 import com.eggmeonina.scrumble.helper.IntegrationTestHelper;
@@ -76,7 +78,7 @@ class SquadTodoAndToDoIntegrationTest extends IntegrationTestHelper {
 		@DisplayName("당일 데이터만 조회할 때_성공")
 		void findSquadTodos_success() {
 			// given
-			SquadTodoRequest request = new SquadTodoRequest(LocalDate.now(), LocalDate.now());
+			SquadTodoRequest request = new SquadTodoRequest(LocalDate.now(), LocalDate.now(), 999999L, 10L);
 
 			// when
 			List<SquadTodoResponse> squadTodos = squadTodoService.findSquadTodos(newSquad.getId(), newMember.getId(),request);
@@ -90,7 +92,7 @@ class SquadTodoAndToDoIntegrationTest extends IntegrationTestHelper {
 		void findSquadTodosWhenTodoDeleted_success() {
 			// given
 			LocalDate date = LocalDate.now().minusDays(2);
-			SquadTodoRequest request = new SquadTodoRequest(date, date);
+			SquadTodoRequest request = new SquadTodoRequest(date, date, 999999L, 10L);
 
 			// when
 			List<SquadTodoResponse> squadTodos = squadTodoService.findSquadTodos(newSquad.getId(), newMember.getId(),request);
@@ -105,13 +107,76 @@ class SquadTodoAndToDoIntegrationTest extends IntegrationTestHelper {
 			// given
 			LocalDate startDate = LocalDate.now().minusDays(1);
 			LocalDate endDate = LocalDate.now();
-			SquadTodoRequest request = new SquadTodoRequest(startDate, endDate);
+			SquadTodoRequest request = new SquadTodoRequest(startDate, endDate, 999999L, 10L);
 
 			// when
 			List<SquadTodoResponse> squadTodos = squadTodoService.findSquadTodos(newSquad.getId(), newMember.getId(),request);
 
 			// then
 			assertThat(squadTodos).hasSize(2);
+		}
+	}
+
+	@Nested
+	@DisplayName("노 오프셋 테스트")
+	class PagingTest{
+
+		@Test
+		@DisplayName("페이지 크기보다 클 때 페이지 크기만큼 조회한다_성공")
+		void whenGreaterThenPageSize_success() {
+			// given
+			Member newMember = createMember("test", "test@test.com", MemberStatus.JOIN, "123234");
+			Squad newSquad = createSquad("스쿼드1", false);
+			ToDo newToDo1 = createToDo(newMember, "프로젝트", TodoStatus.PENDING, false, LocalDate.now());
+			ToDo newToDo2 = createToDo(newMember, "알고리즘", TodoStatus.PENDING, false, LocalDate.now());
+			ToDo newToDo3 = createToDo(newMember, "CS", TodoStatus.PENDING, false, LocalDate.now());
+			SquadToDo newSquadToDo1 = createSquadTodo(newSquad, newToDo1, false);
+			SquadToDo newSquadToDo2 = createSquadTodo(newSquad, newToDo2, false);
+			SquadToDo newSquadToDo3 = createSquadTodo(newSquad, newToDo3, false);
+
+			memberRepository.save(newMember);
+			squadRepository.save(newSquad);
+			todoRepository.saveAll(List.of(newToDo1, newToDo2, newToDo3));
+			squadTodoRepository.saveAll(List.of(newSquadToDo1, newSquadToDo2, newSquadToDo3));
+
+			LocalDate startDate = LocalDate.now().minusDays(1);
+			LocalDate endDate = LocalDate.now();
+			long pageSize = 2L;
+			SquadTodoRequest request = new SquadTodoRequest(startDate, endDate, 99999L, pageSize);
+
+			// when
+			List<SquadTodoResponse> squadTodos = squadTodoService.findSquadTodos(newSquad.getId(), newMember.getId(), request);
+
+			// then
+			assertThat(squadTodos).hasSize(2);
+
+		}
+
+		@Test
+		@DisplayName("페이지 크기보다 적을 때 그 수만큼 조회한다_성공")
+		void whenLessThenPageSize_success() {
+			// given
+			Member newMember = createMember("test", "test@test.com", MemberStatus.JOIN, "123234");
+			Squad newSquad = createSquad("스쿼드1", false);
+			ToDo newToDo1 = createToDo(newMember, "프로젝트", TodoStatus.PENDING, false, LocalDate.now());
+			SquadToDo newSquadToDo1 = createSquadTodo(newSquad, newToDo1, false);
+
+			memberRepository.save(newMember);
+			squadRepository.save(newSquad);
+			todoRepository.saveAll(List.of(newToDo1));
+			squadTodoRepository.saveAll(List.of(newSquadToDo1));
+
+			LocalDate startDate = LocalDate.now().minusDays(1);
+			LocalDate endDate = LocalDate.now();
+			long pageSize = 2L;
+			SquadTodoRequest request = new SquadTodoRequest(startDate, endDate, 99999L, pageSize);
+
+			// when
+			List<SquadTodoResponse> squadTodos = squadTodoService.findSquadTodos(newSquad.getId(), newMember.getId(), request);
+
+			// then
+			assertThat(squadTodos).hasSize(1);
+
 		}
 	}
 
