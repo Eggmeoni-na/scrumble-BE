1d440ccb2c8b6d9cc87d90b0a54393d91c851bde
kimzoo2 <kimzoo1207@gmail.com>
2024-10-17 16:05:51 +0900

    Feat: 알림 도메인 구현

--- src/main/java/com/eggmeonina/scrumble/common/exception/ErrorCode.java
+++ src/main/java/com/eggmeonina/scrumble/common/exception/ErrorCode.java
@@ -45,7 +45,12 @@ public enum ErrorCode {
 	SQUAD_TODO_NOT_FOUND(HttpStatus.BAD_REQUEST, "스쿼드에 속한 투두가 존재하지 않습니다."),
 	TODO_CONTENTS_NOT_BLANK(HttpStatus.BAD_REQUEST, "내용은 비어있을 수 없습니다."),
 	TODO_TYPE_NOT_NULL(HttpStatus.BAD_REQUEST, "투두 타입은 비어있을 수 없습니다."),
-	WRITER_IS_NOT_MATCH(HttpStatus.FORBIDDEN, "투두 작성자가 아닙니다.");
+	WRITER_IS_NOT_MATCH(HttpStatus.FORBIDDEN, "투두 작성자가 아닙니다."),
+
+	/**
+	 * 알림
+	 */
+	NOTIFICATION_TYPE_NOT_NULL(HttpStatus.BAD_REQUEST, "알림 타입은 비어있을 수 없습니다.");
 
 	private final HttpStatus status;
 	private final String message;
