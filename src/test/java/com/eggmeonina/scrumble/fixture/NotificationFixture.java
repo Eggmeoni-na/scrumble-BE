c7e37ec47205df77c40c82106ea5a821ea3f8378
kimzoo2 <kimzoo1207@gmail.com>
2024-10-28 16:07:27 +0900

    Test: 알림 목록 조회 테스트 추가

--- src/test/java/com/eggmeonina/scrumble/fixture/NotificationFixture.java
+++ src/test/java/com/eggmeonina/scrumble/fixture/NotificationFixture.java
@@ -1,29 +1,40 @@
 package com.eggmeonina.scrumble.fixture;
 
+import static com.eggmeonina.scrumble.domain.notification.domain.NotificationType.*;
+
 import java.time.LocalDateTime;
 
 import com.eggmeonina.scrumble.domain.auth.domain.OauthType;
 import com.eggmeonina.scrumble.domain.member.domain.Member;
 import com.eggmeonina.scrumble.domain.member.domain.MemberStatus;
 import com.eggmeonina.scrumble.domain.member.domain.OauthInformation;
-import com.eggmeonina.scrumble.domain.todo.notification.domain.Notification;
-import com.eggmeonina.scrumble.domain.todo.notification.domain.NotificationType;
+import com.eggmeonina.scrumble.domain.notification.domain.Notification;
+import com.eggmeonina.scrumble.domain.notification.domain.NotificationType;
+import com.google.gson.JsonObject;
 
 public class NotificationFixture {
 
 	public static Notification createNotification(Member newMember, NotificationType notificationType,
 		boolean readFlag) {
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("userName", newMember.getName());
+		jsonObject.addProperty("squadName", "테스트 스쿼드");
+
+		if(notificationType.equals(INVITE_REQUEST)){
+			jsonObject.addProperty("squadId", 1L);
+		}
+
 		return Notification.create()
 			.recipient(newMember)
+			.notificationData(jsonObject.toString())
 			.notificationType(notificationType)
-			.readFlag(readFlag)
-			.build();
+			.readFlag(readFlag).build();
 	}
 
-	public static Member createMember(String email, String test, MemberStatus memberStatus) {
+	public static Member createMember(String email, String name, MemberStatus memberStatus) {
 		return Member.create()
 			.email(email)
-			.name(test)
+			.name(name)
 			.memberStatus(memberStatus)
 			.joinedAt(LocalDateTime.now())
 			.oauthInformation(new OauthInformation("1232345", OauthType.GOOGLE))
