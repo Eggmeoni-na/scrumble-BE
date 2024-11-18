d626d9ae5d9faeb129809cfec8d37d0bccb8eebd
kimzoo2 <kimzoo1207@gmail.com>
2024-11-18 15:52:47 +0900

    Feat: 알림 읽기 여부 변경 기능 추가

--- src/main/java/com/eggmeonina/scrumble/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/eggmeonina/scrumble/domain/notification/repository/NotificationRepository.java
@@ -2,13 +2,25 @@ package com.eggmeonina.scrumble.domain.notification.repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.eggmeonina.scrumble.domain.notification.domain.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
 	List<Notification> findAllByRecipientIdAndIdLessThanAndCreatedAtBetweenOrderByIdDesc(Long recipientId, Long id, LocalDateTime startDate, LocalDateTime endDate, Limit limit);
+
+	@Query(
+		"""
+		SELECT n
+		  FROM Notification n
+		 WHERE n.id = :notificationId
+		   AND n.readFlag = false
+		"""
+	)
+	Optional<Notification> findByIdAndReadFlagNot(Long notificationId);
 }
