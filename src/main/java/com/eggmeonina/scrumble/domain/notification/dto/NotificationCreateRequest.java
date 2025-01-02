230662a0478896fdb7e32b8d6bb13677b7026dd2
kimzoo2 <kimzoo1207@gmail.com>
2025-01-03 00:33:58 +0900

    Feat: 알림 생성 요청 DTO 추가

---
+++ src/main/java/com/eggmeonina/scrumble/domain/notification/dto/NotificationCreateRequest.java
@@ -0,0 +1,29 @@
+package com.eggmeonina.scrumble.domain.notification.dto;
+
+import com.eggmeonina.scrumble.domain.member.domain.Member;
+import com.eggmeonina.scrumble.domain.notification.domain.Notification;
+import com.eggmeonina.scrumble.domain.notification.domain.NotificationStatus;
+import com.eggmeonina.scrumble.domain.notification.domain.NotificationType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NotificationCreateRequest {
+	private final Long memberId;
+	private final Long squadId;
+	private final String squadName;
+	private final NotificationType notificationType;
+	private final String notificationData;
+
+	public static Notification to(NotificationCreateRequest request, Member member) {
+		return Notification.create()
+			.recipient(member)
+			.notificationType(request.getNotificationType())
+			.readFlag(false)
+			.notificationData(request.getNotificationData())
+			.notificationStatus(NotificationStatus.PENDING)
+			.build();
+	}
+}
