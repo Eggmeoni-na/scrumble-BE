bd5642279dc5ac11dbca89b8564fa33dc792095a
kimzoo2 <kimzoo1207@gmail.com>
2025-01-15 14:58:25 +0900

    Fix : interceptor 롤백 및 로깅 필터 미적용

--- src/main/java/com/eggmeonina/scrumble/common/config/LoggingFilter.java
+++ src/main/java/com/eggmeonina/scrumble/common/config/LoggingFilter.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
@@ -19,7 +18,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Component
+// @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class LoggingFilter implements Filter {
 	@Override
