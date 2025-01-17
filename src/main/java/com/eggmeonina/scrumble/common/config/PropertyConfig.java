e63ecacec28d35c279a005872bb7223fd52bc227
kimzoo2 <kimzoo1207@gmail.com>
2025-01-18 01:17:04 +0900

    Feat: 프로퍼티 경로 변경
    - profile group 활용하여 자동 바인딩 되도록 수정했음 (config 폴더는 자동 바인딩 됨)

--- src/main/java/com/eggmeonina/scrumble/common/config/PropertyConfig.java
+++ src/main/java/com/eggmeonina/scrumble/common/config/PropertyConfig.java
@@ -2,17 +2,10 @@ package com.eggmeonina.scrumble.common.config;
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import com.eggmeonina.scrumble.ScrumbleApplication;
 
 @Configuration
-@PropertySource(
-	value =
-		{"classpath:/property/google-oauth-${spring.profiles.active}.properties",
-			"classpath:/property/cors-origin.properties"},
-	ignoreResourceNotFound = true
-)
 @ConfigurationPropertiesScan(basePackageClasses = ScrumbleApplication.class)
 public class PropertyConfig {
 }
