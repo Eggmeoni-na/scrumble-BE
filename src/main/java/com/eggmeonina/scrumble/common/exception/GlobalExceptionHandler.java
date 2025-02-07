c9c93078492c7127db49bbafde5e1b8f60efb780
kimzoo2 <kimzoo1207@gmail.com>
2025-02-08 03:46:41 +0900

    Feat: SseEmitter의 IOException 에러 핸들러 추가

--- src/main/java/com/eggmeonina/scrumble/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eggmeonina/scrumble/common/exception/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.eggmeonina.scrumble.common.exception;
 
+import java.io.IOException;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.eggmeonina.scrumble.common.domain.ApiResponse;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,11 +17,29 @@ import lombok.extern.slf4j.Slf4j;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ExpectedException.class)
-	public ResponseEntity<ApiResponse<Void>> customHandlerException(ExpectedException e){
+	public ResponseEntity<ApiResponse<Void>> customHandlerException(ExpectedException e) {
 		this.log(e);
 		return ResponseEntity.status(e.getErrorCode().getStatus())
 			.body(ApiResponse.createErrorResponse(e.getStatusCodeValue(), e.getMessage()));
 
+	}
+
+	/**
+	 * SSEEmitter에서 클라이언트의 연결이 종료되면 Broken pipe 오류가 발생한다.
+	 * Broken pipe 예외를 핸들링하고 이외의 IOException은 에러 로그를 출력한다.
+	 * @param e
+	 * @param req
+	 */
+	@ExceptionHandler(IOException.class)
+	public void handlerIOException(IOException e, HttpServletRequest req) {
+		log.debug("req.requestURI = {}, req.getMethod() = {}", req.getRequestURI(), req.getMethod());
+		if (ExceptionUtils.getRootCauseMessage(e).contains("Broken pipe") &&
+			req.getRequestURI().equals("/api/notifications/subscribe")) {
+			log.warn("broken pipe error!! client close connect = {}", ExceptionUtils.getRootCauseMessage(e));
+			return;
+		}
+		log.error("IOException rootCauseMessage = {}", ExceptionUtils.getRootCauseMessage(e));
+		e.printStackTrace();
 	}
 
 	private void log(ExpectedException e) {
