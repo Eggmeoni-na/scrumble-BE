ceac685f6dc7ba162f596e0bc4a86fd95748f0bd
kimzoo2 <kimzoo1207@gmail.com>
2025-02-13 00:30:27 +0900

    Feat: 나의 스쿼드 조회 조건 추가

--- src/main/java/com/eggmeonina/scrumble/domain/squadmember/repository/SquadMemberRepositoryCustom.java
+++ src/main/java/com/eggmeonina/scrumble/domain/squadmember/repository/SquadMemberRepositoryCustom.java
@@ -2,11 +2,12 @@ package com.eggmeonina.scrumble.domain.squadmember.repository;
 
 import java.util.List;
 
+import com.eggmeonina.scrumble.domain.squadmember.domain.SquadMemberRole;
 import com.eggmeonina.scrumble.domain.squadmember.dto.SquadResponse;
 
 public interface SquadMemberRepositoryCustom {
 
-	List<SquadResponse> findSquads(final Long memberId);
+	List<SquadResponse> findSquads(final Long memberId, SquadMemberRole role);
 
 	boolean existsBySquadMemberNotMemberId(final Long squadId, final Long memberId);
 
