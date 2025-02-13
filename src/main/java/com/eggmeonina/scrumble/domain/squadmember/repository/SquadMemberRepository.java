21a5510fb73b69a454633ccadd2fdf13c89b7857
kimzoo2 <kimzoo1207@gmail.com>
2025-02-13 12:45:11 +0900

    Feat: 회원탈퇴 시 스쿼드 관련 로직 추가
    리더인 경우 (팀원 있으면 회원탈퇴 불가)
    - 스쿼드 탈퇴, 스쿼드 삭제
    
    팀원인 경우
    - 스쿼드 탈퇴

--- src/main/java/com/eggmeonina/scrumble/domain/squadmember/repository/SquadMemberRepository.java
+++ src/main/java/com/eggmeonina/scrumble/domain/squadmember/repository/SquadMemberRepository.java
@@ -1,9 +1,11 @@
 package com.eggmeonina.scrumble.domain.squadmember.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -27,9 +29,25 @@ public interface SquadMemberRepository extends JpaRepository<SquadMember, Long>,
 	List<SquadMember> findBySquadId(final Long squadId);
 
 	@Query("""
+		 SELECT s
+		   FROM SquadMember s
+		  WHERE s.member.id = :memberId AND s.squadMemberStatus = 'JOIN'
+		""")
+	List<SquadMember> findByMemberIdWithJOIN(final Long memberId);
+
+	@Query("""
 		SELECT s
 		  FROM SquadMember s
 		 WHERE s.member.id = :memberId AND s.squad.id = :squadId AND s.squadMemberStatus = 'INVITING'
 		""")
 	Optional<SquadMember> findByMemberIdAndSquadIdWithInvitingStatus(final Long memberId, final Long squadId);
+
+	@Modifying
+	@Query("""
+		UPDATE SquadMember s
+		   SET s.squadMemberStatus = 'LEAVE'
+		     , s.updatedAt = :updatedAt
+		 WHERE s.id in (:squadMemberIds)
+		""")
+	void bulkLeaveSquadMember(List<Long> squadMemberIds, LocalDateTime updatedAt);
 }
